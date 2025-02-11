0aad55b310573423f0bdec3d24d4dc96ebeaa941
GauravDLokhande <132185301+GauravDLokhande@users.noreply.github.com>
2025-02-11 10:58:51 +0530

    Outdated Files Deleted

--- E_Learning_Website_Spingboot/src/main/java/com/elearning/dao/CourseDAO.java
+++
@@ -1,9 +0,0 @@
-package com.elearning.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.elearning.pojos.courses;
-
-public interface CourseDAO extends JpaRepository<courses, Long>{
-
-}
